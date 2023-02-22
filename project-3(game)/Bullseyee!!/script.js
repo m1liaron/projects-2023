423e17b4760709ad0f69fbbb0512ec0bbd5eceb8
vlad <example@gmail.com>
2023-02-22 13:02:11 +0200

    add game Bullseyee!!

---
+++ project-3(game)/Bullseyee!!/script.js
@@ -0,0 +1,413 @@
+window.addEventListener('load',function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1280;
+    canvas.height = 720;
+
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white'
+
+    class Player {
+        constructor(game){
+            this.game = game;
+            this.collisionX = this.game.width * 0.5;
+            this.collisionY = this.game.height * 0.5;
+            // Центральна точка гравця зіткнення 
+            this.collisionRadius = 30;
+            this.speedX = 0;
+            this.speedY = 0;
+            this.dx = 0;
+            this.dy = 0;
+            this.speedModifier = 3;
+            this.spriteWidth = 255;
+            this.spriteHeight = 256;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            //
+            this.spriteX;
+            this.spriteY;
+            // Верхні лівий кут кадру аркуша спрайтів 
+            this.frameX = 0;
+            this.frameY = 0;
+            this.image = document.getElementById('bull');
+            // Ми передаємо таке щоб гравець також не виходив за межи
+            
+        }
+        draw(context){
+            context.drawImage(this.image, this.frameX * this.
+            spriteWidth, this.frameY * this.spriteHeight, 
+            this.spriteWidth, this.spriteHeight, this.spriteX, 
+            this.spriteY, this.width, this.height);
+            if(this.game.debug){
+                context.beginPath();
+            context.arc(this.collisionX,this.collisionY,50,
+                0,Math.PI * 2);
+            //           X   Y  radius початковий кут у радіанах виміряний від позитивна вісьX !! а 5 мне лень если надо 5 то 10.15 !!
+            //Малювання нової форми з щонайменше 5 аргументами
+            context.save();
+            context.globalAlpha = 0.5;
+            context.fill();
+            context.restore();
+            context.stroke();
+            context.beginPath();
+            context.moveTo(this.collisionX, this.collisionY);
+            context.lineTo(this.game.mouse.x,this.game.mouse.y);
+            context.stroke();
+            }
+        }
+        update(){
+            this.dx = this.game.mouse.x - this.collisionX;
+            this.dy = this.game.mouse.y - this.collisionY;
+            // sprite animation
+            const angle =  Math.atan2(this.dy,this.dx);
+            if(angle < -2.74 || angle > 2.74) this.frameY = 6;
+            else if(angle < -1.96) this.frameY = 7;
+            else if(angle < -1.17) this.frameY = 0;
+            else if(angle < -0.39) this.frameY = 1;
+            else if(angle <  0.39) this.frameY = 2;
+            else if(angle <  1.17) this.frameY = 3;
+            else if(angle <  1.96) this.frameY = 4;
+            else if(angle <  2.74) this.frameY = 5;
+
+
+            const distance = Math.hypot(this.dy,this.dx);
+            if(distance > this.speedModifier){
+                this.speedX = this.dx/distance || 0;
+                this.speedY = this.dy/distance || 0;
+            } else{
+                this.speedX = 0;
+                this.speedY = 0;
+            }
+            this.collisionX += this.speedX * this.speedModifier;
+            this.collisionY += this.speedY * this.speedModifier;
+            this.spriteX = this.collisionX - this.width  * 0.5;
+            this.spriteY = this.collisionY - this.height  * 0.5
+            - 100;
+            // collision with obstacles
+            // horizontal boundaries
+            if(this.collisionX <  this.collisionRadius)
+            this.collisionX =  this.collisionRadius;
+            else if(this.collisionX > this.game.width - this.
+            collisionRadius) this.collisionX = this.game.width 
+            - this.collisionRadius; 
+            // vertical boundaries 
+            if(this.collisionY <  this.game.topMargin + this.collisionRadius)
+            this.collisionY =  this.game.topMargin + this.collisionRadius;
+            else if (this.collisionY > this.game.height - this.collisionRadius)
+            this.collisionY = this.game.height - this.collisionRadius;
+            this.game.obstacles.forEach(obstacle => {
+                // [(distance < sumOfRadii), distance, 
+                //     sumOfRadii, dx, dy];
+                let [collision, distance, sumOfRadii, dx, dy] =  
+                this.game.checkCollision(this,obstacle); 
+                // let collision1 = game.checkCollision(this,
+                //     obstacle)[0]
+                // let distance1 = game.checkCollision(this,
+                //     obstacle)[1]
+                if(collision){
+                    const unit_x = dx / distance;
+                    const unit_y = dy / distance;
+                    this.collisionX = obstacle.collisionX + (sumOfRadii + 1) *  unit_x;
+                    // Беремо експозицію центральної точки гравців.Коло зіткнення щоб висунити його за межи радіуса перешкоди, з якою він стискається. Ми рухаємо його горизонтально до положення центру точка кола перешкод плюс сума радіуси кола гравця та перешкоди коло плюс один  додатковий піксель поза ним помножити на одиницю співвідношення Х,щоб дати йому правильному напрямку від перешкоди центрольної точки.
+                    this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y
+                    // Цей код висуває гравця на один піксель за межиі радіус зіткнення з перешкодою в напрямку від центрольної точки 
+                }
+            })
+        }
+    }
+
+    class Obstacle {
+        constructor(game){
+            this.game = game;
+            this.collisionX = Math.random() * this.game.width;
+            this.collisionY = Math.random() * this.game.height;
+            this.collisionRadius = 40;
+            this.image = document.getElementById('obstacles');
+            this.spriteWidth = 250;
+            this.spriteHeight = 250;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height * 0.5
+            - 70;
+            this.frameX = Math.floor(Math.random() * 4);
+            this.frameY = Math.floor(Math.random() * 3);
+         }
+        draw(context){
+            context.drawImage(this.image, this.frameX * this.spriteWidth ,
+                this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.
+                spriteX, this.
+                spriteY, this.width, this.height);
+            // 3 аргумента перше це зобреження 
+            if(this.game.debug){
+                context.beginPath();
+                context.arc(this.collisionX,this.collisionY,this.collisionRadius,
+                    0,Math.PI * 2);
+                //           X   Y  radius початковий кут у радіанах виміряний від позитивна вісьX !! а 5 мне лень если надо 5 то 10.15 !!
+                //Малювання нової форми з щонайменше 5 аргументами
+                context.save();
+                context.globalAlpha = 0.5;
+                context.fill();
+                context.restore();
+            }
+        }
+        update(){
+
+        }
+    }
+
+
+    class Egg {
+        constructor(game){
+            this.game = game;
+            this.collisionRadius = 40;
+            this.margin = this.collisionRadius * 2;
+            this.collisionX = this.margin + (Math.random() * (this.game.width - this.
+            margin * 2));
+            this.collisionY = this.game.topMargin + (Math.random() * (this.game.height
+            - this.game.topMargin - this.margin));
+            this.image = document.getElementById('egg');
+            this.spriteWidth = 110;
+            this.spriteHeight = 135;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.spriteY ; 
+            this.spriteX ;
+        }
+        draw(context){
+            context.drawImage(this.image, this.spriteX, this.
+            spriteY);
+            if(this.game.debug){
+                context.beginPath();
+                context.arc(this.collisionX,this.collisionY,this.collisionRadius,
+                    0,Math.PI  * 2);
+                //           X   Y  radius початковий кут у радіанах виміряний від позитивна вісьX !! а 5 мне лень если надо 5 то 10.15 !!
+                //Малювання нової форми з щонайменше 5 аргументами
+                context.save();
+                context.globalAlpha = 0.5;
+                context.fill();
+                context.restore();
+            }
+        }
+        update(){
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height * 0.5
+            - 30; 
+            let collisionObject = [this.game.player, ...this.
+                game.obstacles, ...this.game.enemies];
+            collisionObject.forEach(object => {
+                let [collision, distance, sumOfRadii, dx, dy]
+                =  this.game.checkCollision(this,object);
+                if(collision){
+                    const unit_x = dx / distance;
+                    const unit_y = dy / distance;
+                    this.collisionX = object.collisionX + 
+                    (sumOfRadii + 1) * unit_x;
+                    this.collisionY = object.collisionY +
+                    (sumOfRadii + 1) * unit_y;
+                }
+            });
+        }
+    }
+
+    class Enemy {
+        constructor(game){
+            this.game = game;
+            this.collisionRadius = 30;
+            this.speedX  = Math.random() * 3 + 0.5;
+            this.image = document.getElementById('toad');
+            this.spriteWidth = 140;
+            this.spriteHeight = 260;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.collisionX = this.game.width + this.width + Math.random() * this.game.width * 0.5;
+            this.collisionY = this.game.topMargin + (Math.random() * (this.game.height - this.game.
+            topMargin));
+            this.spriteX;
+            this.spriteY;
+        }
+        draw(context){
+            context.drawImage(this.image, this.spriteX, this.
+                spriteY);
+                if(this.game.debug){
+                    context.beginPath();
+                    context.arc(this.collisionX,this.collisionY,this.collisionRadius,
+                        0,Math.PI  * 2);
+                    //           X   Y  radius початковий кут у радіанах виміряний від позитивна вісьX !! а 5 мне лень если надо 5 то 10.15 !!
+                    //Малювання нової форми з щонайменше 5 аргументами
+                    context.save();
+                    context.globalAlpha = 0.5;
+                    context.fill();
+                    context.restore();
+                    context.stroke();
+                }
+        }
+        update(){
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height + 40;
+            this.collisionX -= this.speedX;
+             if(this.spriteX + this.width < 0){
+                this.collisionX = this.game.width + this.width
+                + Math.random() * this.game.width * 0.5;  
+                this.collisionY = this.game.topMargin + (Math.random() 
+                * (this.game.height - this.game.topMargin));
+             }
+             let collisionObject = [this.game.player, ...this.
+                game.obstacles];
+            collisionObject.forEach(object => {
+                let [collision, distance, sumOfRadii, dx, dy]
+                =  this.game.checkCollision(this,object);
+                if(collision){
+                    const unit_x = dx / distance;
+                    const unit_y = dy / distance;
+                    this.collisionX = object.collisionX + 
+                    (sumOfRadii + 1) * unit_x;
+                    this.collisionY = object.collisionY +
+                    (sumOfRadii + 1) * unit_y;
+                }
+            });
+        }
+    }
+
+    class Game {
+        constructor(canvas){
+            this.canvas = canvas;
+            this.width = this.canvas.width;
+            this.height = this.canvas.height;
+            // Щоб наша гру була теж самої висоти що ї малюнок^_^
+            this.topMargin = 260;
+            this.debug = true;   
+            this.player  = new Player(this);
+            this.fps = 70;
+            this.timer = 0;
+            // рахує знову і знову від 0 до а конкретне значення,коли воно досягне цього значення, яке запустить наступну анімацію кадру, і його буде скинуто до 0 
+            this.interval = 1000/this.fps;
+            // Значення контрольної точки що при досягненні скине таймер
+            this.eggTimer = 0;
+            this.eggInterval = 1000;
+            this.numberOfObstacles = 10;
+            this.obstacles = [];
+            this.maxEggs = 20 ;
+            this.eggs = [];
+            this.enemies = [];
+            this.gameObjects = [];
+            this.mouse = {
+                x: this.width * 0.5,
+                y: this.height * 0.5,
+                pressed:false
+                //Щоб бачити де знаходеться клік мишки
+            }
+
+            // event listeners
+            canvas.addEventListener('mousedown', e => {
+                this.mouse.x = e.offsetX;   
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = true;
+            });
+            canvas.addEventListener('mouseup', e => {
+                this.mouse.x = e.offsetX;   
+                this.mouse.y = e.offsetY;
+                this.mouse.pressed = false;
+            });
+            canvas.addEventListener('mousemove', e => {
+                if(this.mouse.pressed){
+                    this.mouse.x = e.offsetX;   
+                    this.mouse.y = e.offsetY;
+                }
+            });
+            window.addEventListener('keydown', e => {
+                if(e.key ===  'd') this.debug = !this.debug;
+            });
+        }
+        render(context,deltaTime){ // deltaTime для відтворення методу
+            if(this.timer > this.interval){
+                context.clearRect(0, 0, this.width, this.height);
+                // sort by vertical position
+                this.gameObjects = [this.player, ...this.eggs, ...this.
+                    obstacles, ...this.enemies];
+                    this.gameObjects.sort((a,b) => {
+                        return a.collisionY - b.collisionY;
+                    });
+                this.gameObjects.forEach(object =>  {
+                    object.draw(context);
+                     object.update();
+                });
+                this.timer = 0;
+            }
+            this.timer += deltaTime;
+
+            // add eggs periodically
+            if(this.eggTimer > this.eggInterval && this.eggs
+                .length < this.maxEggs){
+                this.addEgg();
+                this.eggTimer = 0;
+            } else {
+                this.eggTimer += deltaTime;
+            }
+        }
+        checkCollision(a, b){
+            const dx = a.collisionX - b.collisionX;
+            const dy = a.collisionY - b.collisionY;
+            // Різниця між центром кола А і центральна точка кола В на вертикальна вісь у
+            const distance = Math.hypot(dy,dx);
+            const sumOfRadii = a.collisionRadius + b.
+            collisionRadius;
+            return [(distance < sumOfRadii), distance, 
+            sumOfRadii, dx, dy];
+        }
+        addEgg(){
+            this.eggs.push(new Egg(this));
+        }
+        addEnemy(){
+            this.enemies.push(new Enemy(this)); 
+        }
+        init(){
+            for(let i = 0; i < 30; i++){
+                this.addEnemy();
+            }
+            let attempts = 0;
+            while(this.obstacles.length < this.
+                numberOfObstacles && attempts <  500){
+                    let testObstacle = new Obstacle(this);
+                    let overlap = false;
+                    this.obstacles.forEach(obstacle => {
+                        const dx = testObstacle.collisionX - 
+                        obstacle.collisionX;
+                        const dy = testObstacle.collisionY - 
+                        obstacle.collisionY;
+                        const distance = Math.hypot(dy,dx);
+                        const distanceBuffer = 150;
+                        const sumOfRadii = testObstacle.collisionRadius + obstacle.collisionRadius
+                        + distanceBuffer;
+                        if(distance < sumOfRadii){
+                            overlap = true;
+                        }
+                    });
+                    const margin = testObstacle.collisionRadius * 3;
+                    if(!overlap && testObstacle.spriteX > 0 && 
+                        testObstacle.spriteX < this.width - 
+                        testObstacle.width && testObstacle.collisionY > 
+                        this.topMargin + margin && testObstacle
+                        .collisionY < this.height - margin){
+                        this.obstacles.push(testObstacle);
+                    }
+                    attempts++;
+            }
+        }
+    }
+
+    const game = new Game(canvas);
+    game.init();
+    console.log(game);
+
+    let lastTime = 0;
+    function animate(timeStamp){
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+          game.render(ctx,deltaTime);  
+          window.requestAnimationFrame(animate);
+    }
+    animate(0);
+ 
+});
