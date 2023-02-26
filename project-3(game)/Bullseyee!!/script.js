c2b7b7f3e534188b84ed7f44b49f49bba2fcfc90
vlad <example@gmail.com>
2023-02-26 19:06:34 +0200

    Larva sprites and collisions

--- project-3(game)/Bullseyee!!/script.js
+++ project-3(game)/Bullseyee!!/script.js
@@ -249,11 +249,13 @@ window.addEventListener('load',function(){
             this.spriteX;
             this.spriteY;
             this.speedY = 1 + Math.random();
+            this.frameX = 0;
+            this.frameY = Math.floor(Math.random() * 2);
         }
         draw(context){
-            context.drawImage(this.image, 0, 0,this.spriteWidth, 
-            this.spriteHeight, this.spriteX, this.spriteY, this.
-            width, this.height);
+            context.drawImage(this.image, this.frameX * this.spriteWidth, 
+            this.frameY * this.spriteHeight,this.spriteWidth, this.spriteHeight, 
+            this.spriteX, this.spriteY, this.width, this.height);
             if(this.game.debug){
                 context.beginPath();
                 context.arc(this.collisionX,this.collisionY,this.collisionRadius,
@@ -276,7 +278,33 @@ window.addEventListener('load',function(){
             if(this.collisionY < this.game.topMargin){
                 this.markedForDeletion = true;
                 this.game.removeGameObject();
+                this.game.score++;
             }
+            // collision with objects
+            let collisionObject = [this.game.player, ...this.
+                game.obstacles];
+                collisionObject.forEach(object => {
+                let [collision, distance, sumOfRadii, dx, dy]
+                =  this.game.checkCollision(this,object);
+                if(collision){
+                    const unit_x = dx / distance;
+                    const unit_y = dy / distance;
+                    this.collisionX = object.collisionX + 
+                    (sumOfRadii + 1) * unit_x;
+                    this.collisionY = object.collisionY +
+                    (sumOfRadii + 1) * unit_y;
+                   }
+                });  
+                // collision with enemies
+                this.game.enemies.forEach(enemy =>{
+                    if(this.game.checkCollision(this, enemy)[0]){
+                          this.markedForDeletion = true;
+                          this.game.removeGameObject();
+                          this.game.lostHatchlings++;
+                    }else{
+
+                    }
+                })
         }
     }
     
@@ -362,6 +390,8 @@ window.addEventListener('load',function(){
             this.enemies = [];
             this.hatchlings = [];
             this.gameObjects = [];
+            this.scrore = 0;
+            this.lostHatchlings = 0;
             this.mouse = {
                 x: this.width * 0.5,
                 y: this.height * 0.5,
